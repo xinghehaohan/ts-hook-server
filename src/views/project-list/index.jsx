40a532fd683ad2a98e15410ec047659f20e891e7
su <673399718@qq.com>
2022-11-06 21:00:27 +0000

    feat: add custom hooks for userDebounce

--- src/views/project-list/index.jsx
+++ src/views/project-list/index.jsx
@@ -1,41 +1,39 @@
 import React from "react";
-import { useState , useEffect } from "react"
-import { SearchPanel } from "./serch-panel"
-import { List } from "./list"
-import { cleanObject } from "../../utils";
-import qs from "qs"
+import { useState, useEffect } from "react";
+import { SearchPanel } from "./serch-panel";
+import { List } from "./list";
+import { cleanObject, useMount, useDebounce } from "../../utils";
+import qs from "qs";
 const apiUrl = process.env.REACT_APP_API_URL;
-export const ProjectListScreen = ()=>{
-    const [param, setParam] = useState({
-        name:'',
-        personId:''
-    })
-    const [users, SetUsers] = useState([]);
-    const [list, setList] =useState([]);
-    useEffect(()=>{
-        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async(res)=>{
-            if(res.ok){
-                setList(await res.json());
-            }
-        })
-    },[param])
+export const ProjectListScreen = () => {
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const [users, SetUsers] = useState([]);
+  const [list, setList] = useState([]);
+  const debouncedParam = useDebounce(param, 500);
+  useEffect(() => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(
+      async (res) => {
+        if (res.ok) {
+          setList(await res.json());
+        }
+      }
+    );
+  }, [debouncedParam]);
 
-    useEffect(()=>{
-        fetch(`${apiUrl}/users`).then(async(res)=>{
-            if(res.ok){
-                SetUsers(await res.json());
-            }
-        })
-    },[])
-    return <div>
-        <SearchPanel
-            param={param} 
-            setParam={setParam}
-            users={users}
-         />
-        <List 
-        users={users}
-        list={list}
-        />
+  useMount(() => {
+    fetch(`${apiUrl}/users`).then(async (res) => {
+      if (res.ok) {
+        SetUsers(await res.json());
+      }
+    });
+  });
+  return (
+    <div>
+      <SearchPanel param={param} setParam={setParam} users={users} />
+      <List users={users} list={list} />
     </div>
-}
+  );
+};
--- src/views/project-list/list.jsx
+++ src/views/project-list/list.jsx
@@ -1,23 +1,28 @@
 import React from "react";
-export const List= ({users,list})=>{
-    return <div>
-        <table>
-            <thead>
-                <tr>
-                    <th>Project Name</th>
-                    <th>Manager</th>
-                </tr>
-            </thead>
-            <tbody>
-                {list?.map(({id,personId,organization})=>{
-                    return(
-                        <tr key={id}>
-                            <td>{organization}</td>
-                            <td>{users.find(user=>user.id===personId)?.name || 'no user'}</td>
-                        </tr>
-                    )
-                })}
-            </tbody>
-        </table>
+export const List = ({ users, list }) => {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Project Name</th>
+            <th>Manager</th>
+          </tr>
+        </thead>
+        <tbody>
+          {list?.map(({ id, personId, organization }) => {
+            return (
+              <tr key={id}>
+                <td>{organization}</td>
+                <td>
+                  {users.find((user) => user.id === personId)?.name ||
+                    "no user"}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
-}
+  );
+};
